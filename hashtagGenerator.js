35e00a5c9ebc62fb4e5ddd48a38658b5527d4f0d
Robert <robert.d.weston@gmail.com>
2020-03-01 16:33:12 -0800

    Finished hashtag codewars problem

--- hashtagGenerator.js
+++ hashtagGenerator.js
@@ -5,31 +5,24 @@
 console.log("Expected: false\t\t\t\tReturned: " + generateHashtag(""));
 console.log("Expected: false\t\t\t\tReturned: " + generateHashtag(" ".repeat(200)));
 console.log("Expected: #DoWeHaveAHashtag\t\tReturned: " + generateHashtag("Do We have A Hashtag"));
-console.log("Expected: #Codewars\t\t\tReturned: " + generateHashtag("Codewars"));
-console.log("Expected: #CodewarsIsNice\t\tReturned: " + generateHashtag("Codewars is Nice"));
+// console.log("Expected: #Codewars\t\t\tReturned: " + generateHashtag("Codewars"));
+// console.log("Expected: #CodewarsIsNice\t\tReturned: " + generateHashtag("Codewars is Nice"));
 console.log("Expected: #CodeWars\t\t\tReturned: " + generateHashtag("code" + " ".repeat(140) + "wars"));
-
+// console.log("Expected: #A" + "a".repeat(138) + "\t\t\tReturned: " + generateHashtag("a".repeat(139)));
+// console.log("Expected: false\t\t\tReturned: " + generateHashtag("a".repeat(140)));
 
 function generateHashtag(str) { 
-    str = str.trim();
-    // str = str.split(" ");
-    // console.log(str.length);
-    // console.log(str.length);
-    if(str.length == 0) {
+    let lengthTest = str.replace(/\s+/g, '');
+    if(lengthTest.length == 0 || lengthTest.length >= 140) {
         return false;
-    }else {
-        // str = str.split(" ");
-        // console.log(str);
+    } else {
+        str = str.split(" ");
         let hashTag = "#";
         for(let i = 0; i < str.length; i++) {
-            // console.log(str[i].charAt(0));
-            str[i] = str[i].substr(0, 1).toUpperCase() + str[i].substr(1);
-            hashTag += str[i].trim();
+            if(str[i] != ""){
+                hashTag += str[i].substr(0, 1).toUpperCase() + str[i].substr(1);
+            }
         }
-
-        // console.log(str);
-        // console.log(hashTag);
-        // console.log(hashTag);
         return hashTag;
     }
 }
