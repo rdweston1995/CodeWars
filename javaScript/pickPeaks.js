328ecc2aca23b60ebd6984298ad1754e7f8ad72b
Robert <46203504+rdweston1995@users.noreply.github.com>
2020-04-02 23:02:56 -0700

    Working on pickPeaks

--- javaScript/pickPeaks.js
+++ javaScript/pickPeaks.js
@@ -1,14 +1,37 @@
-console.log(pickPeaks([1, 2]));
+// console.log(pickPeaks([1,2,3,6,4,1,2,3,2,1]));
+// console.log(pickPeaks([3,2,3,6,4,1,2,3,2,1,2,3]));
+console.log(pickPeaks([1,2,5,4,3,2,3,6,4,1,2,3,3,4,5,3,2,1,2,3,5,5,4,3]));
+
+//pos: [3,7]
+//peaks: [6, 3]
 
 function pickPeaks(arr) {
-    let peaks = {pos: [], peak: []};
-    // console.log(arr.length);
-    for(let i = 0; i < arr.length ; i++) {
-        if(arr[i] > arr[i - 1] && arr[i] < arr[i + 1]){
-            peaks.peak += arr[i];
-            peaks.pos = i;
+    let pos = [], peaks = [];
+
+    let repeat = 0;
+
+    if(arr.length >= 3) {
+        for(let i = 1; i < arr.length - 1; i++) {
+            if(arr[i] > arr[i - 1]) {
+                if(arr[i] > arr[i  + 1]){
+                    pos.push(i);
+                    peaks.push(arr[i]);
+                } else if(arr[i] === arr[i + 1]){
+                    if(repeat !== i) {repeat === i}
+                    if(arr[repeat] > arr[i + 1]) {
+                        pos.push(i);
+                        peaks.push(arr[i]);
+                    }
+                }
+            } else if(arr[i] === arr[i - 1]){
+                if(repeat !== i) {repeat === i}
+                if(arr[repeat] > arr[i + 1]) {
+                    pos.push(i);
+                    peaks.push(arr[i]);
+                }
+            }
         }
     }
 
-    return peaks;
+    return {pos, peaks};
 }
