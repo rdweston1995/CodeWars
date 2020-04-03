9d7c995c7c85a2c89e0a3042ea736cadcff9f314
Robert <46203504+rdweston1995@users.noreply.github.com>
2020-04-02 21:56:03 -0700

    Working on arr peaks

---
+++ javaScript/add.js
@@ -0,0 +1,12 @@
+/**
+ * Function to add two numbers
+ */
+
+console.log(add("1", "2"));
+
+ function add (n1, n2) {
+    let len; 
+    if(n1 < n2) {len = n1}
+    else {len = n2}
+    
+ }
---
+++ javaScript/pickPeaks.js
@@ -0,0 +1,9 @@
+pickPeaks([1, 2]);
+
+function pickPeaks(arr) {
+    let peak = [], pos = [];
+    console.log(arr.length);
+    for(let i = 0; i < arr.length ; i++) {
+        console.log(arr[i - 1]);
+    }
+}
