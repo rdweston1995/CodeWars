54c5877a4e5ff42929b5bb5d20fe2ec13f2a745d
Robert <46203504+rdweston1995@users.noreply.github.com>
2020-04-02 22:00:40 -0700

    Working on array peaks

--- javaScript/pickPeaks.js
+++ javaScript/pickPeaks.js
@@ -1,9 +1,14 @@
-pickPeaks([1, 2]);
+console.log(pickPeaks([1, 2]));
 
 function pickPeaks(arr) {
-    let peak = [], pos = [];
-    console.log(arr.length);
+    let peaks = {pos: [], peak: []};
+    // console.log(arr.length);
     for(let i = 0; i < arr.length ; i++) {
-        console.log(arr[i - 1]);
+        if(arr[i] > arr[i - 1] && arr[i] < arr[i + 1]){
+            peaks.peak += arr[i];
+            peaks.pos = i;
+        }
     }
+
+    return peaks;
 }
